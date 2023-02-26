9b19d94d8ea792707aba6401cec6b4aca9347ac2
JilianaTiu <jilianatiu@gmail.com>
2023-02-25 23:48:23 -0800

    complete basic customdialog

--- customdialog.js
+++ customdialog.js
@@ -1,15 +1,58 @@
-export function alert() {
-    
-} 
+const alertBtn = document.getElementById('alert');
+const confirmBtn = document.getElementById('confirm');
+const promptBtn = document.getElementById('prompt');
 
-export function confirm() {
-    
-} 
+const dialogAlert = document.getElementById('dialogAlert');
+const dialogConfirm = document.getElementById('dialogConfirm');
+const dialogPrompt = document.getElementById('dialogPrompt');
 
-export function prompt() {
-    
-} 
+const okAlert = document.getElementById('okAlert');
 
-export function saferprompt() {
-    
-} 
+const cancelConfirm = document.getElementById('cancelConfirm');
+const okConfirm = document.getElementById('okConfirm');
+
+const inputPrompt = dialogPrompt.querySelector('input');
+const cancelPrompt = document.getElementById('cancelPrompt');
+const okPrompt = document.getElementById('okPrompt');
+
+const output = document.querySelector('output');
+
+// Alert
+alertBtn.addEventListener('click', () => {
+    dialogAlert.showModal();
+});
+okAlert.addEventListener('click', () => {
+    dialogAlert.close();
+});
+
+// Confirm
+confirmBtn.addEventListener('click', () => {
+    dialogConfirm.showModal();
+});
+cancelConfirm.addEventListener('click', () => {
+    output.innerText = `Confirm result : false`;
+    dialogConfirm.close();
+});
+okConfirm.addEventListener('click', () => {
+    output.innerText = `Confirm result : true`;
+    dialogConfirm.close();
+});
+
+
+promptBtn.addEventListener('click', () => {
+    dialogPrompt.showModal();
+});
+cancelPrompt.addEventListener('click', () => {
+    output.innerText = `User didn't enter anything`;
+    dialogPrompt.close();
+});
+okPrompt.addEventListener('click', () => {
+    if (inputPrompt.value != "") {
+        output.innerText = `Prompt result : ${inputPrompt.value}`;
+    }
+    else {
+        output.innerText = `User didn't enter anything`;
+    }
+    dialogPrompt.close();
+});
+
