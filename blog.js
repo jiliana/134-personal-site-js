ee14a652b3b7b70981a69c460721bd4f934bab38
JilianaTiu <jilianatiu@gmail.com>
2023-02-27 23:34:16 -0800

    update blog js

--- blog.js
+++ blog.js
@@ -71,64 +71,66 @@ form.addEventListener('submit', (event) => {
 function displayBlogArray() {
     blogList.innerHTML = '';
 
-    blogArray.forEach((blog, index) => {
-        const li = document.createElement('li');
-        const h2 = document.createElement('h2');
-        const date = document.createElement('time');
-        const p = document.createElement('p');
-        const editBtn = document.createElement('button');
-        const deleteBtn = document.createElement('button');
+    if (blogArray) {
+        blogArray.forEach((blog, index) => {
+            const li = document.createElement('li');
+            const h2 = document.createElement('h2');
+            const date = document.createElement('time');
+            const p = document.createElement('p');
+            const editBtn = document.createElement('button');
+            const deleteBtn = document.createElement('button');
 
-        h2.textContent = blog.title;
-        date.textContent = blog.date;
-        p.textContent = blog.summary;
-        editBtn.innerHTML = `Edit`;
-        deleteBtn.innerHTML = `Delete`;
+            h2.textContent = blog.title;
+            date.textContent = blog.date;
+            p.textContent = blog.summary;
+            editBtn.innerHTML = `Edit`;
+            deleteBtn.innerHTML = `Delete`;
 
-        editBtn.addEventListener('click', () => {
-            document.getElementById('titleInput').value = blog.title;
-            document.getElementById('dateInput').value = blog.date;
-            document.getElementById('summaryInput').value = blog.summary;
+            editBtn.addEventListener('click', () => {
+                document.getElementById('titleInput').value = blog.title;
+                document.getElementById('dateInput').value = blog.date;
+                document.getElementById('summaryInput').value = blog.summary;
 
-            // target the element at index when submitting form
-            dialog.setAttribute('data-index', index);
-            dialog.showModal();
+                // target the element at index when submitting form
+                dialog.setAttribute('data-index', index);
+                dialog.showModal();
+            });
+
+            deleteBtn.addEventListener('click', () => {
+                // delete element at index
+                // target the element at index when submitting form
+                deleteDialog.setAttribute('data-index', index);
+                deleteDialog.showModal();
+            });
+            li.appendChild(h2);
+            li.appendChild(date);
+            li.appendChild(p);
+            li.appendChild(editBtn);
+            li.appendChild(deleteBtn);
+
+            blogList.appendChild(li);
         });
-
-        deleteBtn.addEventListener('click', () => {
-            // delete element at index
-            // target the element at index when submitting form
-            deleteDialog.setAttribute('data-index', index);
-            deleteDialog.showModal();
-        });
-        li.appendChild(h2);
-        li.appendChild(date);
-        li.appendChild(p);
-        li.appendChild(editBtn);
-        li.appendChild(deleteBtn);
-
-        blogList.appendChild(li);
-    });
-
-    deleteForm.addEventListener('submit', (event) => {
-        event.preventDefault;
-
-        // delete element at index
-        if (deleteDialog.hasAttribute('data-index')) {
-            const index = deleteDialog.getAttribute('data-index');
-            blogArray.splice(index, 1);
-        }
-
-        if (deleteDialog.hasAttribute('data-index')) {
-            deleteDialog.removeAttribute('data-index');
-        }
-
-        localStorage.setItem('blogArray', JSON.stringify(blogArray));
-        displayBlogArray();
-        deleteDialog.close();
-    });
+    }
 
 }
+
+deleteForm.addEventListener('submit', (event) => {
+    event.preventDefault;
+
+    // delete element at index
+    if (deleteDialog.hasAttribute('data-index')) {
+        const index = deleteDialog.getAttribute('data-index');
+        blogArray.splice(index, 1);
+    }
+
+    if (deleteDialog.hasAttribute('data-index')) {
+        deleteDialog.removeAttribute('data-index');
+    }
+
+    localStorage.setItem('blogArray', JSON.stringify(blogArray));
+    displayBlogArray();
+    deleteDialog.close();
+});
 
 
 
